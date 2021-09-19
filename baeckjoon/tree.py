467ce93b419cdc2e2e78ceb5e92918e6845f5466
mlbase <guddnr2424@gamil.com>
2021-09-19 23:38:33 +0900

    linked list and tree

---
+++ baeckjoon/tree.py
@@ -0,0 +1,21 @@
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.right = None
+        self.left = None
+
+    def traverse(self, data):
+        self.
+
+        
+class BinaryTree:
+    
+    def __init__(self, r):
+        self.root = r
+
+if __name__ == '__main__':
+    Node(64)
+    Node.right(19)
+    Node.left(11)
+
